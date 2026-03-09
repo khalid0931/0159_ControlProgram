2ca6a067bea817b1434d737f80f20908583c1f25
khalid0931 <mutiarif53@gmail.com>
2026-03-09 15:41:37 +0700

    membuat int main

--- file1.cpp
+++ file1.cpp
@@ -2,4 +2,16 @@
 using namespace std;
 
 
-int main
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout<<"nilai x sudah di koreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "bilangan yang diberikan = "<<x;
+}
